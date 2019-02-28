9405a0aac7afa5dba483abec64032be4c114a153
rogervieiraa <rogeramorinvieiraa@gmail.com>
2019-02-27 21:20:17 -0300

    image project and tests

---
+++ CombatManager/src/model/City.java
@@ -0,0 +1,45 @@
+package model;
+
+public class City {
+	private String name;
+	private String state;
+	private String country;
+	
+	public City(String name, String state, String country) {
+		this.name = name;
+		this.state = state;
+		this.country = country;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "City [name=" + name + ", state=" + state + ", country=" + country + "]";
+	}
+	
+	
+	
+}
---
+++ CombatManager/src/test/TestCity.java
@@ -0,0 +1,13 @@
+package test;
+
+import model.City;
+
+public class TestCity {
+	
+	public static void main(String[] args) {
+		
+		City city = new City("Criciuma","SC", "Brasil");
+		System.out.println(city.toString());
+		
+	}
+}
