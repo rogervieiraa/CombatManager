b18e07758688f2b4f4fae468fa4cba7962531039
RomuloRPS <romulo.rps@hotmail.com>
2019-05-06 21:53:23 -0300

    Images Modality

--- CombatImage/src/controller/CombatImage.java
+++ CombatImage/src/controller/CombatImage.java
@@ -8,7 +8,12 @@ public final class CombatImage extends ImageIcon {
 		super(CombatImage.class.getResource(title));
 	}
 	
+	public static final CombatImage combat_20x20 = loadImage("/20x20/combat.png");
 	public static final CombatImage alterar_16x16 = loadImage("/16x16/alterar.png");
+	public static final CombatImage localizar_22x22 = loadImage("/22x22/localizar.png");
+	public static final CombatImage adicionar_22x22 = loadImage("/22x22/adicionar.png");
+	public static final CombatImage remover_22x22 = loadImage("/22x22/remover.png");
+	public static final CombatImage salvar_22x22 = loadImage("/22x22/salvar.png");
 	
 	public static CombatImage loadImage(String title) {
 		return new CombatImage(title);
